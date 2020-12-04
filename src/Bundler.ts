738afc75630386552fd2a9079616ea23d0b9ec8a
Dan Buch <dan@meatballhat.com>
2020-12-04 01:33:16 -0500

    Ensure the bundle has a manifest to get the title

--- src/Bundler.ts
+++ src/Bundler.ts
@@ -9,8 +9,8 @@ export class Bundler {
     constructor() {}
 
     public async fromManifest(manifestPath: string): Promise<Bundle> {
-        const bundle = new Bundle(manifestPath)
         const manifest = new Manifest(manifestPath)
+        const bundle = new Bundle(manifestPath, manifest)
         const fileList = [
             path.basename(manifestPath),
         ].concat(Array.from(manifest.files.keys()))
