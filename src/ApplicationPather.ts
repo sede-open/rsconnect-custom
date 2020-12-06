60e1454b504895eeabaf5492efc2f2a3af64de36
Dan Buch <dan@meatballhat.com>
2020-12-05 21:28:57 -0500

    Add debug logging to application pather

--- src/ApplicationPather.ts
+++ src/ApplicationPather.ts
@@ -1,4 +1,6 @@
 import path from 'path'
+
+import { debugLog } from './debugLog'
 import { MiniGit } from './MiniGit'
 
 export class ApplicationPather {
@@ -10,16 +12,21 @@ export class ApplicationPather {
 
   public resolve (manifestPath?: string, appPath?: string): string {
     if (appPath !== null && appPath !== undefined) {
+      debugLog(() => `ApplicationPather: returning non-empty app path=${appPath}`)
       return this.strictAppPath(appPath)
     }
     if (manifestPath === null || manifestPath === undefined) {
+      debugLog(() => 'ApplicationPather: returning empty app path')
       return ''
     }
     const gitTopLevel = this.git.showTopLevel()
     if (gitTopLevel === null) {
-      return this.strictAppPath(path.basename(path.dirname(manifestPath)))
+      const gitLessPath = path.basename(path.dirname(manifestPath))
+      debugLog(() => `ApplicationPather: returning without git prefix removed app path=${gitLessPath}`)
+      return this.strictAppPath(gitLessPath)
     }
     const relPath = path.dirname(manifestPath).replace(gitTopLevel, '')
+    debugLog(() => `ApplicationPather: returning relative to git top level=${gitTopLevel} path=${relPath}`)
     return this.strictAppPath(relPath)
   }
 
